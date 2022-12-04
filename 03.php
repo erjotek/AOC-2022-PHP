2e2f8848e6b741040a334b032ef6276d25f72b6a
Rafał Janiczek <rafal.janiczek@sileman.pl>
2022-12-04 20:07:27 +0100

    optimize

--- 03.php
+++ 03.php
@@ -10,9 +10,7 @@ function part1($input)
     $sum = 0;
     foreach ($input as $i) {
         $c = strlen($i);
-        $w = array_intersect(str_split(substr($i, 0, $c / 2)), str_split(substr($i, $c / 2)));
-        $w = end($w);
-
+        $w = current(array_intersect(str_split(substr($i, 0, $c / 2)), str_split(substr($i, $c / 2))));
         $sum += ord($w) - ($w <= 'Z' ? 38 : 96);
     }
 
@@ -24,8 +22,7 @@ function part2($input)
     $sum = 0;
     $input = array_chunk($input, 3);
     foreach ($input as $i) {
-        $w = array_intersect(str_split($i[0]), str_split($i[1]), str_split($i[2]));
-        $w = end($w);
+        $w = current(array_intersect(str_split($i[0]), str_split($i[1]), str_split($i[2])));
         $sum += ord($w) - ($w <= 'Z' ? 38 : 96);
     }
 
